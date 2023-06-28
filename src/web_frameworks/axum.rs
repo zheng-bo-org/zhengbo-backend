0ad1caed5c29b67ed3bbd0c1a3992e99d969d88d
zhengMa <z1399956473@gmail.com>
2023-06-28 17:30:20 +0800

    remove dupliated folder

--- src/web_frameworks/axum.rs
+++ src/web_frameworks/axum.rs
@@ -7,22 +7,10 @@ use axum::{
 use crate::web_frameworks::web::Web;
 use log::info;
 
-// #[tokio::main]
-// pub async fn start(server: &Server) {
-//     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-//     // run it with hyper on localhost:3000
-//     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-//         .serve(app.into_make_service())
-//         .await
-//         .unwrap();
-// }
-
 #[async_trait]
 impl Web for Server {
     async fn start(self: &Self) {
         let app = Router::new().route("/", get(|| async {
-            info!("Request comming...");
             return "Hello, World!";
         }));
 
