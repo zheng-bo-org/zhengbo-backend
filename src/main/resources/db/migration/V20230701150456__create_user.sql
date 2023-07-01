6a96cf72ca0b1071f272c866448380af994933d2
zhengMa <z1399956473@gmail.com>
2023-07-01 20:04:27 +0800

    add flyway migration file to create tables

--- src/main/resources/db/migration/V20230701150456__create_user.sql
+++ src/main/resources/db/migration/V20230701150456__create_user.sql
@@ -1,24 +1,42 @@
-CREATE TABLE if not exists user
+CREATE TABLE user
 (
     id         BIGINT PRIMARY KEY,
     username   VARCHAR(255),
     pwd        VARCHAR(255),
+    user_type  VARCHAR(255),
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
-CREATE TABLE if not exists school
+CREATE TABLE school
 (
     id           BIGINT PRIMARY KEY,
     name         VARCHAR(255),
     address      VARCHAR(255),
-    min_grade    INTEGER,
-    max_grade    INTEGER,
+    minGrade     INTEGER,
+    maxGrade     INTEGER,
     school_level VARCHAR(255),
     created_at   TIMESTAMP,
     updated_at   TIMESTAMP
 );
 
-CREATE TABLE if not exists student
+CREATE TABLE role
+(
+    id         BIGINT PRIMARY KEY,
+    role       VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+CREATE TABLE user_role
+(
+    id         BIGINT PRIMARY KEY,
+    user_id    BIGINT,
+    role_id    BIGINT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
+);
+CREATE TABLE student
 (
     id         BIGINT PRIMARY KEY,
     user_id    BIGINT,
